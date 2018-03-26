766f464ebc30294cfd67cef459d1e50422472e77
georgiana17 <georgiana.nicolae17@gmail.com>
2018-03-26 23:15:56 +0300

    Commit no 2
    Addind index.html, templates, app.js and server.js

---
+++ public/app.js
@@ -0,0 +1,71 @@
+var app = angular.module("tvTracker",['ngMaterial','ngMdIcons','ngRoute'])
+app.controller("AppController", function($scope,$http, $mdSidenav){
+    $scope.firstName = "TvTracker";
+    $scope.menuItems = [
+        {
+            link: '/',
+            title: 'Home',
+            icon: 'home'
+        },
+        {
+            link: 'http://google.es',
+            title: 'My TV Shows',
+            icon: 'favorite'
+        },
+        {
+            link: 'http://google.es',
+            title: 'Calendar',
+            icon: 'date_range'
+        },
+        {
+            link: 'http://google.es',
+            title: 'Settings',
+            icon: 'settings'
+        }
+        
+    ]
+    $scope.userItems = [
+        {
+            link: '/topSeries',
+            title: 'Top Series',
+            icon: 'star'
+        },
+        {
+            link: 'http://google.es',
+            title: 'Recommendations',
+            icon: 'thumb_up'
+        }
+    ]
+
+    $scope.getPopularTvSeries = function() {
+        var popularData = $http.get("/popular").then(function(response){
+            $scope.popularSeries = response.data.results;
+            console.log($scope.popularSeries);
+        });
+        console.log(popularData)
+        return popularData;
+    }
+    $scope.getPopularTvSeries();
+   })
+app.config(function($mdThemingProvider, $mdIconProvider, $routeProvider, $locationProvider) {
+    $mdThemingProvider.theme('default')
+      .primaryPalette('deep-purple')
+      .accentPalette('orange');
+
+    $mdIconProvider.icon('md-toggle-arrow', 'img/icons/toggle-arrow.svg', 48);
+
+    // $locationProvider.html5Mode({
+    //     enabled: true,
+    //     requireBase: false
+    // });
+    $locationProvider.hashPrefix(''); 
+    
+    $routeProvider
+    .when("/", {
+        templateUrl: "views/main.html"
+    })
+    .when("/topSeries",{
+        templateUrl: "views/topRated.html"
+    })
+    
+  })
---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var path = require("path");
+var bodyParser = require("body-parser");
+var fetch = require('isomorphic-fetch');
+
+require('dotenv').config({path: 'access_keys.env'})
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.json());
+
+app.get("/", function(req, res) {
+  res.sendFile('index.html' , { root : path.join(__dirname, "public")});
+});
+
+var greysUrl = `https://api.themoviedb.org/3/tv/top_rated?api_key=${process.env.TMDB_KEY}&language=en-US&page=1`;
+
+app.get('/topSeries', function (req, res) {
+  fetch(`${greysUrl}`)
+      .then(response => response.json())
+      .then(movie => res.send(movie))
+      .catch(error => res.send(error))
+});
+
+app.listen(3000);
+
+//    background-image: radial-gradient(circle at 20% 50%, rgba(76.86%, 6.27%, 41.96%, 0.98) 0%, rgba(43.92%, 3.92%, 56.86%, 0.88) 100%);
