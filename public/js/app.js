a5e6a91c0573f8f4f593691e1b65bd4e105148c6
georgiana17 <georgiana.nicolae17@gmail.com>
2018-05-03 23:31:59 +0300

    replaced angular-material-calendar with ui-calendar
    calendar-md is not responsive, and not showing all div from calendar with all episodes.
    TODO: tooltip for ui-calendar-event :D

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module("tvTracker",['ngMaterial','ngMdIcons','ngRoute','ngMessages','materialCalendar'])
+var app = angular.module("tvTracker",['ngMaterial','ngMdIcons','ngRoute','ngMessages','materialCalendar', 'ui.calendar'])
 app.controller("AppController", function($scope, $http, $mdSidenav, $mdDialog, $location, $rootScope, auth, session) {
     $scope.appName = "TvTracker";
     $rootScope.loggedIn = false;
